6308f41338868667ea35f833e96ba5d6de5dba72
Brenda SC <brenda.saavedra@fondeadora.com>
2023-04-08 20:46:15 -0600

    Initial Commit

---
+++ TodoList_SwiftUI_ChatGPT/TodoList_SwiftUI_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_SwiftUI_ChatGPTApp.swift
+//  TodoList_SwiftUI_ChatGPT
+//
+//  Created by Brenda Saavedra  on 08/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_SwiftUI_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
